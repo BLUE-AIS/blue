5d892e6f7ad85630499cd1e9012966816b44b8c4
BLUE-AIS <ruddy.walon.59300@gmail.com>
2025-01-10 14:16:11 +0100

    Update 01.1_create_env_pve.sh

--- 01.1_create_env_pve.sh
+++ 01.1_create_env_pve.sh
@@ -1,20 +1,4 @@
-#!/bin/sh
-# Variables communes
-TEMPLATE_DIR="/root/cloud-version"
-STORAGE_POOL="local-lvm"
-BRIDGE="vmbr0"
-CORES=2
-MEMORY=2048
-DISK_SIZE="10G"
-CLOUDINIT_DISK="${STORAGE_POOL}:cloudinit"
-# Création de répertoires et de l'arborescence de travail
-mkdir -p "$TEMPLATE_DIR"
-cd "$TEMPLATE_DIR"
-# Fonction pour créer un template
-create_template() {
-    local id=$1
-    local name=$2
-    local url=$3
+  GNU nano 8.2                                                                         01.1_create_env_pve.sh *                                                                                
     local img_file=$(basename "$url")
     echo "Création du template $name"
     mkdir -p "$name"
@@ -37,19 +21,15 @@ create_template() {
 pvesh create /pools --poolid zone-relais --comment "zone Relais"
 pvesh create /pools --poolid zone-exposee --comment "zone exposée"
 pvesh create /pools --poolid zone-interne --comment "zone service interne"
-pvesh create /pools --poolid zone-testing --comment "zone fireworld""
+pvesh create /pools --poolid zone-testing --comment "zone parefeu"
 
 # Templates 
 create_template 9001 "debian.template"  "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
 create_template 9002 "alma.template"    "https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-9.5-20241120.x86_64.qcow2"
-create_template 9003 "windows.template" "https://image.cloud.example.com/windows_server_2019.iso"
-create_template 9004 "opnsense.template" "https://image.cloud.example.com/opnsense.iso"
-
-
+create_template 9003 "windows.template" 
+create_template 9004 "opnsense.template" 
 pvesh set /pools/zone.templates --vm 9001
 pvesh set /pools/zone.templates --vm 9002
 pvesh set /pools/zone.templates --vm 9003
 pvesh set /pools/zone.templates --vm 9004
-
-
-echo "Fin de création du paramétrage de bases de  proxmox"
+echo "Fin de création du paramétrage de bases de proxmox"
